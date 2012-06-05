7131e8663c883ebb55b387d928844f687170d2e4
Jarosluv <hello@jaroslove.ru>
2012-06-05 22:04:09 +0400

    Какой-то коммит

---
+++ protected/models/Secretary.php
@@ -0,0 +1,13 @@
+<?php
+
+class Secretary {
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'approve' => 'Утвердить',
+        );
+    }
+}
